0d57bd0a72da8b757f7475c0961ba7a93577860e
João Gabriel Lopes Aguiar <joaogabriellopesaguiar@MacBook-Air-de-Joao.local>
2025-04-13 18:34:27 -0300

    fix: correct Speed Insights import and initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { inject } from '@vercel/speed-insights'
 import { Analytics } from "@vercel/analytics/react"
 import { testConnection } from './database/db'
 
 // Testar conexão com o banco de dados
 testConnection()
 
+// Inicializa o Speed Insights
+inject()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
     <Analytics />
   </React.StrictMode>,
 )
